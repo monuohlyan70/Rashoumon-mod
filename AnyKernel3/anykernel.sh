IamCexary/kernel_xiaomi_sm6375

AnyKernel3: Dont tag version number
it gets annoying changing it again and again

AnyKernel3/anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Rashoumon Kernel 1.0 by Cexary
+kernel.string=Rashoumon Kernel by Cexary
 do.devicecheck=0
 do.modules=0
 do.systemless=1

monuohlyan70/Rashoumon-mod

AnyKernel3/anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Rashoumon Kernel 1.0 by Cexary
+kernel.string=Redline Kernel by Hanamizaki
 do.devicecheck=0
 do.modules=0
 do.systemless=1
290d5d2ab49a6d80135caa727c24482875a3ed74
IamCexary <kajirokasuna@gmail.com>
2024-03-19 13:32:14 +0530

    AnyKernel3: Dont tag version number
    it gets annoying changing it again and again

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Rashoumon Kernel 1.0 by Cexary
+kernel.string=Redline Kernel by Hanamizaki
 do.devicecheck=0
 do.modules=0
 do.systemless=1
